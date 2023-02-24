5124dd9d1ec8a62d19c7e1fb9cd111a1d1a12776
Amy Spears <amspears007@gmail.com>
2023-02-24 10:00:06 -0700

    Add method for name and books in Author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,17 @@
 class Author
+  attr_reader :first_name, 
+              :last_name,
+              :books
 
-  
+  def initialize(info)
+    @first_name = info[:first_name]
+    @last_name = info[:last_name]
+    @books = []
+  end
+
+  def name
+    "#{@first_name} #{ @last_name}"
+  end
+
+
 end
