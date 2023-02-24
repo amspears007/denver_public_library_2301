c48b31af2a11c945ff11c7a7151be26eec89a82e
Amy Spears <amspears007@gmail.com>
2023-02-24 09:37:30 -0700

    Add test and method for author in book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,18 @@
 class Book
   attr_reader :title, 
-              :author, 
+              :author_first_name,
+              :author_last_name,
               :publication_date
   
   def initialize(info)
     @title = info[:title]
-    @author = info[:author_first_name]
+    # require 'pry'; binding.pry
+    @author_first_name = info[:author_first_name]
+    @author_last_name = info[:author_last_name]
     @publication_date = info[publication_date]
+  end
+
+  def author
+    "#{@author_first_name} #{ @author_last_name}"
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
-RSpec.describe Book do
-  it 'exists and has a title' do
-  book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
+RSpec.describe Book do
+  let(:book) { Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})}
+
+  it 'exists and has a title' do
   expect(book).to be_a(Book)
   expect(book.title).to eq("To Kill a Mockingbird")
+  end
+
+  it 'has a an author and a publication year' do
+    expect(book.author).to eq("Harper Lee")
   end
 end
